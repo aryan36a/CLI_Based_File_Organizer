0e2693a999e0f5976ff4dabf5738021d5e0c337c
Aryan Sangave <123397860+aryan36a@users.noreply.github.com>
2026-03-29 05:49:00 +0530

    alpha

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cctype>
+#include<filesystem>
+#include<string>
+namespace fs=std::filesystem; 
+int main()
+{
+    std::string
+    std::cout<<"Enter Path to be organized: "
+
+return 0;
+}
