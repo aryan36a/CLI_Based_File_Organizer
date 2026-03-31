5a5f1501c72471c5fff828c85adbd828ef959b90
Aryan Sangave <123397860+aryan36a@users.noreply.github.com>
2026-03-31 09:40:32 +0530

    added basic sorting logic (alpha)(untested)

--- src/organizer.cpp
+++ src/organizer.cpp
@@ -3,44 +3,43 @@
 #include<filesystem>
 #include"organizer.h"
 #include<string>
-
+#include<unordered_map>
 namespace fs=std::filesystem;
+
+//Map basic extensions
+std::string getCat(const std::string extension) {
+    static std::unordered_map<std::string, std::string> mapping={
+        {".jpg","images"},{".png","image"},{".jpeg","image"},
+        {".pdf","document"},{".doc","document"},{".docx","document"},
+        {".mp4","video"},{".mkv","video"}
+    };
+    if(mapping.count(extension)){
+        return mapping[extension];
+    }
+    return "others";
+}
 
 void organize(const fs::path& sourcePath){
     int count=0;
-    //Map Extensions
-    std::string getCat(const std::string extension) {
-        static std::unordered_map<std::string, std::string> mapping={
-            {".jpg","images"},{".png","image"},{".jpeg","image"},
-            {".pdf","document"},{".doc","document"},{".docx","document"},
-            {".mp4","video"},{".mkv","video"}
-        }
-        if (mapping.count(extension)){
-            return mapping[extension];
-        }
-        return "others";
-    }
-
     try{
-        for(const auto& entry :fs::recursive_directory_iterator(sourcePath)){
-            if(entry.is_regular_file()){
-                count++;
+        for(const auto& entry :fs::directory_iterator(sourcePath)){
+            if(!fs::is_regular_file(entry.path())){
+                continue;
             }
+            //Get extension
+            std::string ext=entry.path().extension().string();
+            //get file type
+            std::string type=getCat(ext);
+            //create parth
+            fs::path targetPath=sourcePath/type;
+            //create dir if does not exist
+            if(!fs::exists(targetPath)){
+                fs::create_directory(targetPath);
+            }
+            //move file
+            fs::rename(entry.path(),targetPath/entry.path().filename());
         }
-    }catch (const fs::filesystem_error& e){
-        std::cerr<<"Error while processing the directory\n";
-        return;
+    }catch(const fs::filesystem_error& e){
+        std::cerr<<"Error occured during organizing: "<<e.what()<<"\n";
     }
-    std::cout<<"Total number of files: "<<count<<"\n";
-    std::string finalPath=sourcePath.string()+R"(\sorted)";
-    fs::create_directory(finalPath);
-    for(const auto& entry :fs::directory_iterator(sourcePath)){
-        if(entry.is_regular_file()){
-            if(entry.path().extension()==".txt"){
-                fs::create_directory(finalPath+R"(text files)");
-            }        
-        }
-    }
-
-    return;
 }
