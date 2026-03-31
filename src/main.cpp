99d032397b1ab2e296682aa5bf24bac6f64c37ec
Aryan Sangave <123397860+aryan36a@users.noreply.github.com>
2026-03-31 10:43:29 +0530

    performance improvements and fixed logic

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,28 @@ int main(int argc, char *argv[])
     //Checks argument count
     if(argc!=2){
         std::cerr<<"Usage: "<<argv[0]<<" <folder_path>";
-        return 0;
+        return 1;
     }
     fs::path sourcePath=argv[1];
     
     //check if path exists
     if (!fs::exists(sourcePath)){
         std::cout<<"The folder doesnt exist\n";
-        return 0;
+        return 1;
     }
 
     //checks if path is to a folder or not
     if(fs::is_regular_file(sourcePath)){
         std::cout<<"The path points to a file not a folder\n";
-        return 0;
+        return 1;
     }
 
     //checks accessibility
     try{
-        fs::recursive_directory_iterator(sourcePath);
+        fs::directory_iterator(sourcePath);
     }catch (const fs::filesystem_error& e){
         std::cerr<<"Error: Cannot access the directory\n";
+        return 1;
     }
      
     //normalize path
@@ -39,5 +40,5 @@ int main(int argc, char *argv[])
 
     //organizer call
     organize(sourcePath);
-return 0;
+return 1;
 }
